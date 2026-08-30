921606151e1b9a32e15943528739333d37e01d8a
Ramin Poursakha <poursakharamin@gmail.com>
2026-08-30 19:52:09 +0200

    Add property creation form

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => redirect()->route('properties.index'));
 
 Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
-Route::get('/properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
+    Route::get('/properties/create', [PropertyController::class, 'create'])->name('properties.create');
+    Route::post('/properties', [PropertyController::class, 'store'])->name('properties.store');
 
+    Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
     Route::get('/my-bookings', [BookingController::class, 'index'])->name('bookings.index');
     Route::post('/properties/{property}/bookings', [BookingController::class, 'store'])->name('bookings.store');
     Route::patch('/bookings/{booking}/cancel', [BookingController::class, 'cancel'])->name('bookings.cancel');
@@ -21,5 +21,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
 
 require __DIR__.'/auth.php';
